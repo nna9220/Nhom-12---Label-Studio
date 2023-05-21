86d553c806369dbe92a94bcb477da22d326bcc1a
nna9220 <102048288+nna9220@users.noreply.github.com>
2023-05-21 11:30:30 +0700

    Delete label_studio directory

--- label_studio/tasks/exceptions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import status
-
-from core.utils.exceptions import LabelStudioAPIException
-
-
-class AnnotationDuplicateError(LabelStudioAPIException):
-    status_code = status.HTTP_409_CONFLICT
-    default_detail = 'Annotation with this unique id already exists'
